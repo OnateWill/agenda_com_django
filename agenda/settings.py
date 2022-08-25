526fa53b308d1d0d0fc23e3e64b54b761e49a780
Won <onatewill42@gmail.com>
2022-08-25 11:55:56 -0300

    Criado tabela de eventos, para trabalhar com a agenda

--- agenda/settings.py
+++ agenda/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6nn&dzr#0s*e=ur148_uu7ol758s$48(jg1y&_ho&yhmchg=(1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
