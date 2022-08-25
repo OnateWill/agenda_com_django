834129b058d20d56396abeba5ba64858f116e52c
Won <onatewill42@gmail.com>
2022-08-25 19:09:18 -0300

    Criada Tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
 
@@ -16,3 +17,6 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
+
+
+
+"""
+outro meio de fazer o redirect da pagina vazia
+def index(request):
+    retur redirect('/agenda/')
+"""
